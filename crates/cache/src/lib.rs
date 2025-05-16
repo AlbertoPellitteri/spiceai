8df9899948d10e0626b23239c80a7a88d86fb352
peasee <98815791+peasee@users.noreply.github.com>
2025-05-16 18:39:35 +1000

    fix: Avoid cloning cached results (#5853)
    * fix: Avoid cloning cached RecordBatches
    
    * wip
    
    * wip
    
    * wip
    
    * chore: Clippy
    
    * fix: Remove old comment
    
    * fix: Retain Arc<RecordBatch>

--- crates/cache/src/lib.rs
+++ crates/cache/src/lib.rs
@@ -27,14 +27,19 @@ use std::time::UNIX_EPOCH;
 
 use arrow::array::RecordBatch;
 use arrow::datatypes::Schema;
+use arrow::datatypes::SchemaRef;
 use async_trait::async_trait;
 use byte_unit::Byte;
 use datafusion::common::ParamValues;
+use datafusion::error::DataFusionError;
+use datafusion::execution::RecordBatchStream;
 use datafusion::execution::SendableRecordBatchStream;
 use datafusion::logical_expr::LogicalPlan;
 use datafusion::scalar::ScalarValue;
 use datafusion::sql::TableReference;
 use fundu::ParseError;
+use futures::Stream;
+use futures::task::{Context, Poll};
 use lru_cache::LruCache;
 use snafu::{ResultExt, Snafu};
 use spicepod::component::runtime::ResultsCache;
@@ -151,6 +156,54 @@ pub struct CachedQueryResult {
     pub records: Arc<Vec<RecordBatch>>,
     pub schema: Arc<Schema>,
     pub input_tables: Arc<HashSet<TableReference>>,
+}
+
+pub struct CachedStream {
+    /// Vector of record batches
+    data: Arc<Vec<RecordBatch>>,
+    /// Schema representing the data
+    schema: SchemaRef,
+    index: usize,
+}
+
+impl CachedStream {
+    #[must_use]
+    pub fn try_new(data: Arc<Vec<RecordBatch>>, schema: SchemaRef) -> Self {
+        Self {
+            data,
+            schema,
+            index: 0,
+        }
+    }
+}
+
+impl Stream for CachedStream {
+    type Item = Result<RecordBatch, DataFusionError>;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        _: &mut Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        Poll::Ready(if self.index < self.data.len() {
+            let index = self.index;
+            let batch = self.data.get(index).cloned().map(Ok);
+            self.index += 1;
+            batch
+        } else {
+            None
+        })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.data.len(), Some(self.data.len()))
+    }
+}
+
+impl RecordBatchStream for CachedStream {
+    /// Get the schema
+    fn schema(&self) -> SchemaRef {
+        Arc::clone(&self.schema)
+    }
 }
 
 #[async_trait]
